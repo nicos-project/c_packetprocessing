6387c8fea5eb2636477d57e967e18cabb970430f
rana-97 <kr36747@my.utexas.edu>
2025-10-13 13:17:18 -0500

    Turning the interface up after loading the firmware

--- apps/nat/build_and_load.sh
+++ apps/nat/build_and_load.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 make clean && make && sudo ./init/load.sh restart nat.fw
+
+sleep 5
+
+# Turn on the interfaces so that the link comes up
+# TODO: Is there a way to make this setting so that we don't have to
+# do this each time after reloading the firmware. With the old BSP
+# on pikachu, we didn't need this
+sudo nfp -m mac -e set port ifup 0 0
+sudo nfp -m mac -e set port ifup 0 4
