e9712b67b788cfbfb36e2e92208a18a64ae3029a
rana-97 <kr36747@my.utexas.edu>
2025-10-20 10:11:29 -0500

    Updated the libraries in NAT and echo with the ones in flowenv; Bringing up the interfaces after loading the echo fw

--- apps/echo/build_and_load.sh
+++ apps/echo/build_and_load.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 make clean && make && sudo ./init/load.sh restart echo.fw
+
+# Turn on the interfaces so that the link comes up
+# TODO: Is there a way to make this setting so that we don't have to
+# do this each time after reloading the firmware. With the old BSP
+# on pikachu, we didn't need this
+sudo nfp -m mac -e set port ifup 0 0
+sudo nfp -m mac -e set port ifup 0 4
