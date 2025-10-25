e63ba43536cec3e0cb7a68ec4be86ffebfde8927
rana-97 <kr36747@my.utexas.edu>
2025-10-25 14:20:59 -0500

    Added worker choice in the build script too

--- apps/packet_steer/build_and_load.sh
+++ apps/packet_steer/build_and_load.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-make clean && make && sudo ./init/load.sh restart steer.fw
+# Default to echo if no argument provided
+WORKER=${1:-echo}
+
+# Validate the worker type
+if [[ "$WORKER" != "echo" && "$WORKER" != "nat" ]]; then
+    echo "Error: Invalid worker type '$WORKER'"
+    echo "Usage: $0 [echo|nat]"
+    exit 1
+fi
+
+echo "Building with WORKER=$WORKER"
+
+make clean && make WORKER=$WORKER && sudo ./init/load.sh restart steer.fw
 
 # Turn on the interfaces so that the link comes up
-# TODO: Is there a way to make this setting so that we don't have to
+# TODO: Is there a way to make this setting permanent so that we don't have to
 # do this each time after reloading the firmware. With the old BSP
 # on pikachu, we didn't need this
 sudo nfp -m mac -e set port ifup 0 0
