9909f9adc1cc9ee5beb45308d7db8c5fa9813c60
rana-97 <kr36747@my.utexas.edu>
2025-11-09 16:38:46 -0600

    Made the stateful NAT static allocation based

--- apps/stf_nat/nat.h
+++ apps/stf_nat/nat.h
@@ -1,41 +1,5 @@
 #ifndef __NAT_H__
 #define __NAT_H__
-// 48 bits in total
-// LAN to WAN table key
-// The value is a WAN port
-struct nat_ltw_lkup_key {
-    union {
-        struct {
-            uint32_t ip_src;
-            uint16_t l4_src;
-            uint16_t __unused;
-        };
-        struct {
-            uint32_t word[2];
-        };
-        uint64_t word64;
-    };
-};
-
-// WAN to LAN key is the UDP destination port on the incoming packet on the
-// WAN port. The value is an index in an array of nat_wtl_lkup_value
-// structures which stores the IP and port that need to be swapped. Since we
-// don't have the entries timing out, the WAN port allocation during LAN to WAN
-// conversion is always sequential and we don't need a map (either a CAM based
-// or SW based) for looking it up.
-struct nat_wtl_lkup_value {
-    union {
-        struct {
-            uint32_t dest_ip;
-            uint16_t port;
-            uint8_t valid;
-        };
-        struct {
-            uint32_t word[2];
-        };
-        uint64_t word64;
-    };
-};
 
 // Avoiding the well-known ports (0-1023)
 #define WAN_PORT_START 1024
@@ -46,10 +10,36 @@ struct nat_wtl_lkup_value {
 
 #define WAN_IP_HEX 0x36EF1C55 // 54.239.28.85
 
-#define NAT_LTW_TABLE_NUM_BUCKETS  (1 << 14)
-#define TABLE_SZ_64    (NAT_LTW_TABLE_NUM_BUCKETS * 64)
-#define NAT_LTW_TABLE_MEM __emem
-#define DATA_OFFSET 0
-#define NAT_LTW_TABLE_MAX_KEYS_PER_BUCKET 6
+#define NAT_LTW_TABLE_MAX_ENTRIES_PER_BUCKET 4
+#define NAT_LTW_TABLE_NUM_BUCKETS 0x4000
+#define NAT_LTW_TABLE_ID_MASK 0x3fff
+
+struct nat_ltw_bucket_entry {
+    uint32_t four_tuple_hash;
+    uint16_t wan_port;
+};
+
+struct nat_ltw_bucket {
+    struct nat_ltw_bucket_entry entry[NAT_LTW_TABLE_MAX_ENTRIES_PER_BUCKET];
+};
+
+// WAN to LAN key is the UDP destination port on the incoming packet on the
+// WAN port. The value is an index in an array of nat_wtl_lkup_value
+// structures which stores the IP and port that need to be swapped.
+// Change the name to something else for this
+struct nat_wtl_lkup_value {
+    union {
+        struct {
+            uint32_t dest_ip;
+            uint16_t port;
+            // Why do we need the valid parameter?
+            uint8_t valid;
+        };
+        struct {
+            uint32_t word[2];
+        };
+        uint64_t word64;
+    };
+};
 
 #endif /* __NAT_H__ */
--- apps/stf_nat/nat_worker.c
+++ apps/stf_nat/nat_worker.c
@@ -15,8 +15,65 @@
 #include "steer.h"
 #include "nat.h"
 
-__import NAT_LTW_TABLE_MEM struct mem_lkup_cam_r_48_64B_table_bucket_entry nat_ltw_lkup_table[NAT_LTW_TABLE_NUM_BUCKETS];
-__import __emem struct nat_wtl_lkup_value nat_wtl_lkup_table[WAN_PORT_POOL_SIZE];
+// Global NAT state
+__declspec(imem export scope(global)) struct nat_ltw_bucket nat_ltw_lkup_table[NAT_LTW_TABLE_NUM_BUCKETS];
+__declspec(imem export scope(global)) uint8_t ltw_bucket_count[NAT_LTW_TABLE_NUM_BUCKETS];
+__declspec(imem export scope(global)) struct nat_wtl_lkup_value nat_wtl_lkup_table[WAN_PORT_POOL_SIZE];
+__declspec(imem export scope(island) aligned(64)) int nat_per_island_sem = 1;
+__declspec(imem export scope(island)) uint16_t cur_wan_port = 0;
+
+void semaphore_down(volatile __declspec(mem addr40) void * addr)
+{
+    /* semaphore "DOWN" = claim = wait */
+    unsigned int addr_hi, addr_lo;
+    __declspec(read_write_reg) int xfer;
+    SIGNAL_PAIR my_signal_pair;
+
+    addr_hi = ((unsigned long long int)addr >> 8) & 0xff000000;
+    addr_lo = (unsigned long long int)addr & 0xffffffff;
+
+    do {
+        xfer = 1;
+        __asm {
+            mem[test_subsat, xfer, addr_hi, <<8, addr_lo, 1], \
+                sig_done[my_signal_pair];
+            ctx_arb[my_signal_pair]
+        }
+    } while (xfer == 0);
+}
+
+void semaphore_up(volatile __declspec(mem addr40) void * addr)
+{
+    /* semaphore "UP" = release = signal */
+    unsigned int addr_hi, addr_lo;
+    __declspec(read_write_reg) int xfer;
+
+    addr_hi = ((unsigned long long int)addr >> 8) & 0xff000000;
+    addr_lo = (unsigned long long int)addr & 0xffffffff;
+
+    __asm {
+        mem[incr, --, addr_hi, <<8, addr_lo, 1];
+    }
+}
+
+__intrinsic int32_t find_in_nat_ltw_table(uint32_t hash_value, uint32_t table_idx) {
+    __gpr uint32_t cur_idx = 0;
+    __gpr int32_t wan_port = -1;
+    while (cur_idx < NAT_LTW_TABLE_MAX_ENTRIES_PER_BUCKET) {
+        if (nat_ltw_lkup_table[table_idx].entry[cur_idx].four_tuple_hash == hash_value) {
+            wan_port = nat_ltw_lkup_table[table_idx].entry[cur_idx].wan_port;
+            break;
+        }
+        else if (nat_ltw_lkup_table[table_idx].entry[cur_idx].four_tuple_hash == 0) {
+            // we initialize all entries to zero in the start
+            // and add them sequentially, so if we found a 0 entry
+            // we can break since there are no entries further down
+            break;
+        }
+        cur_idx++;
+    }
+    return wan_port;
+}
 
 int main(void)
 {
@@ -32,9 +89,10 @@ int main(void)
         __xread  struct work_t work_read;
 
         // NAT stuff
-        __gpr int i;
+        __gpr int i, j;
         __gpr uint32_t lan_or_wan;
-        __gpr uint16_t wan_port = 0;
+        __gpr uint32_t hash_value;
+        __gpr int32_t wan_port = -1;
         __xread struct nbi_meta_catamaran nbi_meta;
         __xread struct nbi_meta_pkt_info *pi = &nbi_meta.pkt_info;
         __declspec(ctm shared) __mem40 char *pbuf;
@@ -48,11 +106,19 @@ int main(void)
         // NAT LTW table stuff
         __gpr uint32_t table_idx;
         __gpr uint16_t nat_wtl_lkup_idx = 0;
-        __declspec(local_mem shared) struct nat_ltw_lkup_key ltw_lkup_key;
-        __declspec(local_mem shared) unsigned long nat_ltw_lkup_key_shf;
-        __declspec(local_mem shared) uint64_t nat_ltw_lkup_data; // this is what actually goes in the CAM (right-shifted result of ltw_lkup_key.word64 by nat_ltw_lkup_key_shf)
-        __xrw uint32_t nat_ltw_lkup_key_result[2];
         SIGNAL work_sig;
+
+        // Initialize the LAN to WAN table
+        for (i = 0; i < NAT_LTW_TABLE_NUM_BUCKETS; i++) {
+            for (j = 0; j < NAT_LTW_TABLE_MAX_ENTRIES_PER_BUCKET; j++) {
+                nat_ltw_lkup_table[i].entry[j].four_tuple_hash = 0;
+                nat_ltw_lkup_table[i].entry[j].wan_port = 0;
+            }
+        }
+
+        for (i = 0; i < WAN_PORT_POOL_SIZE; i++) {
+            nat_wtl_lkup_table[i].word64 = 0;
+        }
 
         island = __ISLAND;
 
@@ -72,8 +138,6 @@ int main(void)
           rnum = MEM_RING_GET_NUM(flow_ring_3);
           raddr_hi = MEM_RING_GET_MEMADDR(flow_ring_3);
         }
-
-        nat_ltw_lkup_key_shf = MEM_LKUP_CAM_64B_KEY_OFFSET(DATA_OFFSET, sizeof(nat_ltw_lkup_table));
 
         for (;;) {
             __mem_workq_add_thread(rnum, raddr_hi,
@@ -109,27 +173,33 @@ int main(void)
             ip_src = ip_hdr->src;
 
             if (lan_or_wan == 0) {
-                // Now perform a lookup in the LAN to WAN table
-                ltw_lkup_key.word64 = 0;
-                ltw_lkup_key.ip_src = ip_hdr->src;
-                ltw_lkup_key.l4_src = *l4_src_port;
 
-                ltw_lkup_key.word[1] = work.hash;
-                nat_ltw_lkup_key_result[0] = ltw_lkup_key.word[1];
-                nat_ltw_lkup_key_result[1] = ltw_lkup_key.word[0];
+                hash_value = work.hash;
+                table_idx = hash_value & NAT_LTW_TABLE_ID_MASK;
 
-                // NAT TABLE LOOKUP OPERATIONS
-                mem_lkup_cam_r_48_64B(nat_ltw_lkup_key_result, (__mem40 void *) nat_ltw_lkup_table,
-                                      DATA_OFFSET, sizeof(nat_ltw_lkup_key_result),
-                                      sizeof(nat_ltw_lkup_table));
+                if (tcp_hdr->flags & NET_TCP_FLAG_SYN) {
+                    // grab a lock and add to the NAT LTW and WTL tables
+                    semaphore_down(&nat_per_island_sem);
 
-                if (nat_ltw_lkup_key_result[0]) {
-                    // key was found in the CAM
-                    wan_port = nat_ltw_lkup_key_result[0];
+                    nat_ltw_lkup_table[table_idx].entry[ltw_bucket_count[table_idx]].four_tuple_hash = hash_value;
+                    nat_ltw_lkup_table[table_idx].entry[ltw_bucket_count[table_idx]].wan_port = cur_wan_port;
+                    ltw_bucket_count[table_idx]++;
+                    wan_port = cur_wan_port++;
+
+                    nat_wtl_lkup_table[wan_port - WAN_PORT_START].dest_ip = ip_hdr->src;
+                    nat_wtl_lkup_table[wan_port - WAN_PORT_START].port = *l4_src_port;
+                    nat_wtl_lkup_table[wan_port - WAN_PORT_START].valid = 0x1;
+
+                    semaphore_up(&nat_per_island_sem);
                 }
                 else {
-                    // This should never happen
-                    *data = 0xffffff;
+                    // find the wan port in the NAT table
+                    // Now perform a lookup in the LAN to WAN table
+                    wan_port = find_in_nat_ltw_table(hash_value, table_idx);
+                    if (wan_port == -1) {
+                        // This should never happen
+                        *data = 0xffffff;
+                    }
                 }
 
                 // PACKET UPDATE OPERATIONS
